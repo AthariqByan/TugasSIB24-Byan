1ce57ad79e16a1933c857cd93e9af4dc4edcc4c9
AthariqByan <fadhlansantoso25@gmail.com>
2024-03-11 16:11:57 +0700

    First commit

---
+++ js/jsdasar.js
@@ -0,0 +1,13 @@
+// Struktur Code
+//* Menampilkan Alert
+function tampilkanAlert() {
+    return alert("Hello semua");
+}
+
+var fullName = "Byan";
+document.write(fullName);
+document.writeln("<br>");
+
+fullName = "Athariq Byan";
+document.write(fullName);
+document.writeln("<br>");
